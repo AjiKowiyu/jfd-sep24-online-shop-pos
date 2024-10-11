dc37fc5162ef1879e0ed3fec46e09f9caf396017
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-10-11 19:42:52 +0700

    proses login part 1

--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,3 +1,5 @@
+const model_user = require('../model/m_user')
+
 module.exports =
 {
 
@@ -7,12 +9,22 @@ module.exports =
 
 
 
-    proses_login: function(req,res) {
+    proses_login: async function(req,res) {
         // ambil inputan dari form login
         let form_email      = req.body.form_email
         let form_password   = req.body.form_password
 
         // cek email yg diinput, ada gak di db
+        let email_exist = await model_user.cari_email(form_email)
+
+        if (email_exist.length > 0) {
+            // cek password
+            res.send('email ada')
+        } else {
+            // tendang ke halaman register
+            let pesan = `Email anda belum terdaftar, silakan registrasi lebih dulu!`
+            res.redirect(`/auth/login?notif=${pesan}`)
+        }
     }
 
 }
